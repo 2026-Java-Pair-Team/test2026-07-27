878f7231822e66552698279a16212ee4f8dd7074
ayato <ayato.211210@gmail.com>
2026-07-28 11:07:43 +0900

    new src main numberGame

--- src/project1/NumberGame.java
+++ src/project1/NumberGame.java
@@ -3,7 +3,9 @@ package project1;
 
 public class NumberGame {
 	public String judge(int num) {
-		 if (num % 2 == 0) {
+		if (num == 777) {
+			return "ラッキーセブンです";
+		} else if (num % 2 == 0) {
 			return "偶数です";
 		} else {
 			return "奇数です";
